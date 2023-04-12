d8758f49e4ce20e78d67edef67bd0e2cec6055a6
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2023-04-12 10:50:53 +0700

    fix css login signup

--- FE/src/pages/Signup.tsx
+++ FE/src/pages/Signup.tsx
@@ -3,133 +3,70 @@ import "./styles/pages.scss";
 
 export default function Signup() {
     return (
-        <div>
-            <main>
-                <div className="container">
-                    <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                                    <div className="d-flex justify-content-center py-4">
-                                        <a
-                                            href="assets/book.img"
-                                            className="logo d-flex align-items-center w-auto"
-                                        >
-                                            <img src="assets/img/logo.png" />
-                                            <span className="d-none d-lg-block">
-                                                My Book Store
-                                            </span>
-                                        </a>
-                                    </div>
-                                    {/* End Logo */}
-                                    <div className="card mb-3">
-                                        <div className="card-body">
-                                            <div className="pt-4 pb-2">
-                                                <h5 className="card-title text-center pb-0 fs-4">
-                                                    Create an Account
-                                                </h5>
-                                                <p className="text-center small">
-                                                    Enter your personal details
-                                                    to create account
-                                                </p>
-                                            </div>
-                                            <form
-                                                className="row g-3 needs-validation"
-                                                noValidate
-                                            >
-                                                <div className="col-12">
-                                                    <label
-                                                        htmlFor="yourEmail"
-                                                        className="form-label"
-                                                    >
-                                                        Email
-                                                    </label>
-                                                    <input
-                                                        type="email"
-                                                        name="email"
-                                                        className="form-control"
-                                                        id="yourEmail"
-                                                        required
-                                                    />
-                                                    <div className="invalid-feedback">
-                                                        Please enter a valid
-                                                        Email adddress!
-                                                    </div>
-                                                </div>
-                                                <div className="col-12">
-                                                    <label
-                                                        htmlFor="yourUsername"
-                                                        className="form-label"
-                                                    >
-                                                        Username
-                                                    </label>
-                                                    <div className="input-group has-validation">
-                                                        <span
-                                                            className="input-group-text"
-                                                            id="inputGroupPrepend"
-                                                        >
-                                                            @
-                                                        </span>
-                                                        <input
-                                                            type="text"
-                                                            name="username"
-                                                            className="form-control"
-                                                            id="yourUsername"
-                                                            required
-                                                        />
-                                                        <div className="invalid-feedback">
-                                                            Please choose a
-                                                            username.
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="col-12">
-                                                    <label
-                                                        htmlFor="yourPassword"
-                                                        className="form-label"
-                                                    >
-                                                        Password
-                                                    </label>
-                                                    <input
-                                                        type="password"
-                                                        name="password"
-                                                        className="form-control"
-                                                        id="yourPassword"
-                                                        required
-                                                    />
-                                                    <div className="invalid-feedback">
-                                                        Please enter your
-                                                        password!
-                                                    </div>
-                                                </div>
-                                                <br />
-
-                                                <div className="col-12 margin-signup">
-                                                    <button
-                                                        className="btn btn-primary w-100"
-                                                        type="submit"
-                                                    >
-                                                        Create Account
-                                                    </button>
-                                                </div>
-                                                <div className="col-12 margin-signup">
-                                                    <p className="small mb-0">
-                                                        Already have an account?{" "}
-                                                        <a href="pages-login.html">
-                                                            Log in
-                                                        </a>
-                                                    </p>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+        <div className="h-100 mt-5 align-items-center ">
+            <div className="d-flex  justify-content-center h-100">
+                <div className="user_card">
+                    <div className="d-flex justify-content-center">
+                        <div className="brand_logo_container">
+                            <img
+                                src="assects/logo.jpg"
+                                className="brand_logo"
+                                alt="Logo"
+                            />
                         </div>
-                    </section>
+                    </div>
+                    <div className="d-flex justify-content-center form_container">
+                        <form>
+                            <div className="input-group mb-3">
+                                <div className="input-group-append">
+                                    <span className="input-group-text">
+                                        <i className="fas fa-user" />
+                                    </span>
+                                </div>
+                                <input
+                                    type="text"
+                                    className="form-control input_user"
+                                    placeholder="username"
+                                />
+                            </div>
+
+                            <div className="input-group mb-2">
+                                <div className="input-group-append">
+                                    <span className="input-group-text">
+                                        <i className="fas fa-key" />
+                                    </span>
+                                </div>
+                                <input
+                                    type="password"
+                                    className="form-control input_pass"
+                                    placeholder="password"
+                                />
+                            </div>
+
+                            <div className="d-flex justify-content-center mt-3 login_container">
+                                <button
+                                    type="button"
+                                    name="button"
+                                    className="btn login_btn"
+                                >
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="mt-4">
+                        <div className="d-flex justify-content-center links">
+                            Don't have an account?{" "}
+                            <a href="#" className="ml-2">
+                                Sign Up
+                            </a>
+                        </div>
+                        <div className="d-flex justify-content-center links">
+                            <a href="#">Forgot your password?</a>
+                        </div>
+                    </div>
                 </div>
-            </main>
-            {/* End #main */};
+            </div>
         </div>
     );
 }
