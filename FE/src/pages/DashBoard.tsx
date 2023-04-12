1b81919799d8215adfc2d02cec809a85517f0642
thong89x <thong89x@gmail.com>
2023-04-12 22:57:07 +0700

    add filter years

--- FE/src/pages/DashBoard.tsx
+++ FE/src/pages/DashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -13,6 +13,7 @@ import { Bar, Pie } from 'react-chartjs-2';
 import BarChart from '@/components/dashboard/BarChart';
 import PieChart from '@/components/dashboard/PieChart';
 import TotalMoney from '@/components/dashboard/TotalMoney';
+import ToTalAmout from '@/components/dashboard/ToTalAmout';
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -23,11 +24,30 @@ ChartJS.register(
     Legend
 );
 export default function DashBoard() {
+    const [curYearChoosen,setYear] = useState(new Date().getFullYear())
+    const arroundYears = [curYearChoosen+1,curYearChoosen,curYearChoosen-1];
+    useEffect(() => {
+      
+    
+      return () => {
+        
+      }
+    }, [curYearChoosen])
     
     return (
         <div className='dash-board'> 
-            <div className='row'>
-            
+            <div className='row d-flex flex-wrap justify-content-between'>
+                <ToTalAmout title='Tổng sách nhập kho' amount='85000' iconClass="fa-solid fa-book" bg_color={{left:'darkorchid',right:'pink'}}/>
+                <ToTalAmout title='Tổng đơn hàng' amount='32' iconClass="fa-solid fa-cart-shopping" bg_color={{left:'#2fafe2',right:'#88ebe2'}}/>
+                <select value={curYearChoosen} onChange={(e)=>{
+                        const num =parseInt(e.currentTarget.value);
+                        setYear(pre=>num)
+                        
+                    }} className="custom-select filter-year" id="inputGroupSelect02">
+                    <option value={arroundYears[0]}>{arroundYears[0]}</option>
+                    <option value={arroundYears[1]}>{arroundYears[1]}</option>
+                    <option value={arroundYears[2]}>{arroundYears[2]}</option>
+                </select>
             </div>
             <div className='row d-flex flex-wrap justify-content-between'>
                 <TotalMoney title='Tổng tiền nhập kho' amount='13,750,000' iconClass="fa-solid fa-truck" bg_color={{left:'red',right:'lightcoral'}}/>
@@ -38,7 +58,7 @@ export default function DashBoard() {
                 <div className='chart-card'>
                     <BarChart data={[100,500,200,100,8000,400,1000,500,200,100,8000,400]}/>
                 </div>
-                <div className='chart-card ml-5'>
+                <div className='chart-card ml-4'>
                     <PieChart data = {[12, 19, 3, 5, 2, 3]} cate={["Romatic","Action","War","Novel","Fatasy","Cartoon"]}/>
                 </div>
             </div>
