951c6fd8209f0f38034bc3eb6f39183e47cdd2f9
hieucckha <hieucckha@gmail.com>
2023-04-17 21:21:09 +0700

    Fix CORS

--- BE/Api/App/CorsConfiguration.cs
+++ BE/Api/App/CorsConfiguration.cs
@@ -6,20 +6,16 @@ public static class CorsConfiguration
     {
         // var origins = new string[]
         // {
-        //     // "http://localhost:8000",
-        //     // "https://tayyentu-api.stlsolution.com"
-        //     "http://localhost:5173",
         // };
-        services.AddCors(options => options.AddPolicy("CorsPolicy",
-            builder =>
-            {
-                builder
-                    // .WithOrigins(origins)
-                    // .SetIsOriginAllowed(_ => true)
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            }));
+        services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+        {
+            builder
+                // .WithOrigins(origins)
+                // .SetIsOriginAllowed(_ => true)
+                // .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        }));
     }
 }
